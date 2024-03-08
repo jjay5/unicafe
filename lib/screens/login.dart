6277395853c3c850fa236e80177bb8c3f4eadbb7
Jaharah <jaharahjainari@gmail.com>
2024-03-08 11:40:06 +0800

    update login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:unicafe/screens/seller/update_seller.dart';
-import 'package:unicafe/services/auth_service.dart';
+import 'package:unicafe/screens/customer/update_customer.dart';
 
-class LoginPage extends StatelessWidget {
-  final AuthService _auth = AuthService();
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  LoginPage({super.key});
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -18,41 +27,22 @@ class LoginPage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextField(
               controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-              ),
+              decoration: const InputDecoration(labelText: 'Email'),
             ),
+            const SizedBox(height: 16.0),
             TextField(
               controller: _passwordController,
               obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-              ),
+              decoration: const InputDecoration(labelText: 'Password'),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 16.0),
             ElevatedButton(
-              onPressed: () async {
-                dynamic result = await _auth.signIn(_emailController.text, _passwordController.text);
-                if (result != null) {
-                  // Navigate to the HomePage if login is successful
-                  Navigator.pushReplacement( // or Navigator.push for non-replacement
-                    context,
-                    MaterialPageRoute(builder: (context) => const UpdateSellerPage()),
-                  );
-                } else {
-                  // Display error message if login fails
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Login Failed. Please check your email and password.'),
-                      duration: Duration(seconds: 3),
-                    ),
-                  );
-                }
+              onPressed: () {
+                _login();
               },
               child: const Text('Login'),
             ),
@@ -60,5 +50,52 @@ class LoginPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> _login() async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+
+      // Retrieve user role from Firestore
+      String role = await _getUserRole(userCredential.user!.uid);
+
+      // Navigate based on role
+      if (role == 'customer') {
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const UpdateCustomerPage()));
+      } else if (role == 'seller') {
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const UpdateSellerPage()));
+      } else {
+        // Handle unknown role
+      }
+    } catch (e) {
+      // Handle login errors
+      print('Login Error: $e');
+      // Show error message to the user
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Failed to login. Please check your credentials.'),
+      ));
+    }
+  }
+
+  Future<String> _getUserRole(String userId) async {
+    try {
+      DocumentSnapshot userDoc = await _firestore.collection('customer').doc(userId).get();
+      if (userDoc.exists) {
+        return 'customer';
+      }
+
+      userDoc = await _firestore.collection('seller').doc(userId).get();
+      if (userDoc.exists) {
+        return 'seller';
+      }
+
+      return ''; // Unknown role
+    } catch (e) {
+      print('Error fetching user role: $e');
+      return ''; // Return empty string if an error occurs
+    }
   }
 }
