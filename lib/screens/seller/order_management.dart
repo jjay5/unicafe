b632a8ab4e044cd73f43ed368f63e59f930cd365
Jaharah <jaharahjainari@gmail.com>
2024-05-13 06:49:07 +0800

    +notes

--- lib/screens/seller/order_management.dart
+++ lib/screens/seller/order_management.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:unicafe/models/order.dart';
+import 'package:unicafe/screens/seller/order_details_seller.dart';
 
 class OrderManagementPage extends StatefulWidget {
   final String sellerID;
@@ -8,22 +9,32 @@ class OrderManagementPage extends StatefulWidget {
   const OrderManagementPage({super.key, required this.sellerID});
 
   @override
-  _OrderManagementPageState createState() => _OrderManagementPageState();
+  OrderManagementPageState createState() => OrderManagementPageState();
 }
 
-class _OrderManagementPageState extends State<OrderManagementPage> {
-  late Stream<List<Orders>> _ordersStream;
+class OrderManagementPageState extends State<OrderManagementPage> {
+  late Stream<List<Orders>> _pendingOrdersStream;
+  late Stream<List<Orders>> _preparingOrdersStream;
+  late Stream<List<Orders>> _readyToPickupOrdersStream;
+  late Stream<List<Orders>> _completedOrdersStream;
 
   @override
   void initState() {
     super.initState();
-    _ordersStream = FirebaseFirestore.instance
+    _pendingOrdersStream = _getOrdersStream('pending');
+    _preparingOrdersStream = _getOrdersStream('preparing');
+    _readyToPickupOrdersStream = _getOrdersStream('ready to pickup');
+    _completedOrdersStream = _getOrdersStream('completed');
+  }
+
+  Stream<List<Orders>> _getOrdersStream(String status) {
+    return FirebaseFirestore.instance
         .collection('orders')
         .where('sellerID', isEqualTo: widget.sellerID)
+        .where('orderStatus', isEqualTo: status)
         .snapshots()
         .map((snapshot) => snapshot.docs.map((doc) => Orders.fromFirestore(doc)).toList());
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -31,25 +42,125 @@ class _OrderManagementPageState extends State<OrderManagementPage> {
       appBar: AppBar(
         title: const Text('Order Management'),
       ),
-      body: StreamBuilder<List<Orders>>(
-        stream: _ordersStream,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
-          final orders = snapshot.data ?? [];
-          return ListView.builder(
-            itemCount: orders.length,
-            itemBuilder: (context, index) {
-              final order = orders[index];
-              return OrderListItem(order: order);
-            },
-          );
-        },
+      body: DefaultTabController(
+        length: 4,
+        child: Column(
+          children: [
+            const TabBar(
+              tabs: [
+                Tab(
+                  child: Text(
+                    'Pending',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12, // Adjust the font size as needed
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    'Preparing',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12, // Adjust the font size as needed
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    'Ready to \nPickup',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12, // Adjust the font size as needed
+                    ),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    'Completed',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 12, // Adjust the font size as needed
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            // Header
+            const SizedBox(height: 15),
+            const Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Text('Order ID',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16, // Underlined like a hyperlink
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Text('Items',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16, // Underlined like a hyperlink
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Text('Order Status',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16, // Underlined like a hyperlink
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Divider(),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  _buildOrdersList(_pendingOrdersStream),
+                  _buildOrdersList(_preparingOrdersStream),
+                  _buildOrdersList(_readyToPickupOrdersStream),
+                  _buildOrdersList(_completedOrdersStream),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildOrdersList(Stream<List<Orders>> stream) {
+    return StreamBuilder<List<Orders>>(
+      stream: stream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        }
+        final orders = snapshot.data ?? [];
+        return ListView.builder(
+          itemCount: orders.length,
+          itemBuilder: (context, index) {
+            final order = orders[index];
+            return OrderListItem(order: order);
+          },
+        );
+      },
     );
   }
 }
@@ -57,73 +168,93 @@ class _OrderManagementPageState extends State<OrderManagementPage> {
 class OrderListItem extends StatelessWidget {
   final Orders order;
 
-  const OrderListItem({required this.order});
+  const OrderListItem({super.key, required this.order});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text('Order ID: ${order.id}'),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text('Items:'),
-          StreamBuilder<List<OrderItem>>(
-            stream: order.getOrderItems(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              }
-              if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              }
-              final orderItems = snapshot.data ?? [];
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: orderItems
-                    .map((item) => Text(
-                  '- ${item.menuItem.itemName}, Quantity: ${item.quantity}',
-                ))
-                    .toList(),
-              );
-            },
-          ),
-          const SizedBox(height: 8),
-          Row(
+    return GestureDetector(
+        onTap: () {
+          // Navigate to order detail page
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => OrderDetailPage(order: order),
+            ),
+          );
+        },
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text('Order Status: '),
-              DropdownButton<String>(
-                value: order.orderStatus,
-                onChanged: (newValue) {
-                  // Update order status
-                  _updateOrderStatus(context, order, newValue!);
-                },
-                items: ['pending', 'preparing', 'ready to pickup', 'completed']
-                    .map<DropdownMenuItem<String>>((status) {
-                  return DropdownMenuItem<String>(
-                    value: status,
-                    child: Text(status),
-                  );
-                }).toList(),
+              // Order details
+              Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Text(
+                      '${order.id}',
+                      //textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        decoration: TextDecoration.underline, // Underlined like a hyperlink
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: StreamBuilder<List<OrderItem>>(
+                      stream: order.getOrderItems(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          return const CircularProgressIndicator();
+                        }
+                        if (snapshot.hasError) {
+                          return Text('Error: ${snapshot.error}');
+                        }
+                        final orderItems = snapshot.data ?? [];
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: orderItems
+                              .map((item) => Text(
+                            '${item.quantity}x ${item.menuItem.itemName} \n${item.notes}\n',
+                          ))
+                              .toList(),
+                        );
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: DropdownButton<String>(
+                      value: order.orderStatus,
+                      onChanged: (newValue) {
+                        // Update order status
+                        _updateOrderStatus(context, order, newValue!);
+                      },
+                      items: ['pending', 'preparing', 'ready to pickup', 'completed']
+                          .map<DropdownMenuItem<String>>((status) {
+                        return DropdownMenuItem<String>(
+                          value: status,
+                          child: Text(status),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ],
               ),
+              // Divider
+              const Divider(),
             ],
           ),
-        ],
-      ),
-      onTap: () {
-        // Navigate to order detail page
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => OrderDetailPage(order: order),
-          ),
-        );
-      },
+        )
     );
   }
 
   void _updateOrderStatus(BuildContext context, Orders order, String newStatus) {
-    // Implement logic to update order status here
+
     order.updateOrderStatus(newStatus);
+
     // Show a success message or handle errors as needed
     ScaffoldMessenger.of(context).showSnackBar(
       const SnackBar(content: Text('Order status updated successfully!')),
@@ -131,78 +262,5 @@ class OrderListItem extends StatelessWidget {
   }
 }
 
-class OrderDetailPage extends StatelessWidget {
-  final Orders order;
 
-  const OrderDetailPage({required this.order});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Order Details'),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text('Order ID: ${order.id}'),
-          const SizedBox(height: 8),
-          const Text('Items:'),
-          StreamBuilder<List<OrderItem>>(
-            stream: order.getOrderItems(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const CircularProgressIndicator();
-              }
-              if (snapshot.hasError) {
-                return Text('Error: ${snapshot.error}');
-              }
-              final orderItems = snapshot.data ?? [];
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: orderItems
-                    .map((item) => Text(
-                  '- ${item.menuItemId}, Quantity: ${item.quantity}',
-                ))
-                    .toList(),
-              );
-            },
-          ),
-          const SizedBox(height: 8),
-          Text('Order Status: ${order.orderStatus}'),
-          const SizedBox(height: 8),
-          ElevatedButton(
-            onPressed: () {
-              _updateOrderStatus(context, order, 'preparing');
-            },
-            child: const Text('Update Order Status to Preparing'),
-          ),
-          const SizedBox(height: 8),
-          ElevatedButton(
-            onPressed: () {
-              _updateOrderStatus(context, order, 'ready to pickup');
-            },
-            child: const Text('Update Order Status to Ready to Pickup'),
-          ),
-          const SizedBox(height: 8),
-          ElevatedButton(
-            onPressed: () {
-              _updateOrderStatus(context, order, 'completed');
-            },
-            child: const Text('Update Order Status to Completed'),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void _updateOrderStatus(BuildContext context, Orders order, String newStatus) {
-    // Implement logic to update order status here
-    order.updateOrderStatus(newStatus);
-    // Show a success message or handle errors as needed
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('Order status updated successfully!')),
-    );
-  }
-}
 
