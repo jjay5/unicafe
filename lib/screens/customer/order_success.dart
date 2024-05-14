75b5c2a15379794b6cddcf4cb3d651390eeeed4f
Jaharah <jaharahjainari@gmail.com>
2024-05-14 16:43:10 +0800

    order success page

---
+++ lib/screens/customer/order_success.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:unicafe/screens/customer/my_order.dart';
+
+class OrderSuccessPage extends StatelessWidget {
+  const OrderSuccessPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Order Successful'),
+        automaticallyImplyLeading: false,
+
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Icon(
+              Icons.check_circle_outline,
+              color: Colors.green,
+              size: 100.0,
+            ),
+            const SizedBox(height: 20.0),
+            const Text(
+              'Your order has been placed successfully!',
+              style: TextStyle(fontSize: 24.0),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) =>  const CustomerOrdersPage()),
+                );
+              },
+              child: const Text('View My Order'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
